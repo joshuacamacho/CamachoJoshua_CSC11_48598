acf21c0439e898d401f7b8f850d29aa296ca0a80
Josh <joshuajcamacho@gmail.com>
2014-10-11 16:46:08 -0700

    234542354252

--- Hmwk/assignment3/prog.s
+++ Hmwk/assignment3/prog.s
@@ -1,5 +1,5 @@
-	.global _start
-_start:
+	.global main
+main:
 	MOV r2, #111
 	MOV r3, #5
 	MOV r4, #0
@@ -9,31 +9,35 @@ _start:
 	MOV r8, #10
 	MOV r9, #0
 	MOV r1, r2
+divmod:
 	CMP r1, r3
-	BLT _swap
-_divmod:
+	BGT scale
+scale:
 	MOV r6, #1
-	MLA r7, r3, r6 @ r0 = r1 * r2
-	MLA r9, r7, r8
-	CMP r1, r9
-	BGT _scale
-_scale:
-	ADD r6, r6, #10
 	MLA r7, r3, r6
 	MLA r9, r7, r8
 	CMP r1, r9
-	BGT _scale
-_div:
+	BGT scale2
+scale2:
+	MLA r6, r6, r8
+	MLA r7, r3, r6
+	MLA r9, r7, r8
+	CMP r1, r9
+	BGT scale2
+div:
 	ADD r0, r0, r6
 	SUB r1, r1, r7
 	CMP r1, r7
-	BGE _divmod
+	BGE div
+	CMP r6, #1
+	BGT scale
+	CMP r4, #1
+	BEQ last
+	BAL end
+last
+	MOV r5, r0
+	MOV r0, r1
+	MOV r1, r5
 end:
 	MOV r7, #1
 	SWI 0
-
-
-
-
-
-
