55b06666e05628d86c9dcce6b566914e228b68a6
root <root@raspberrypi.(none)>
2014-10-12 00:44:18 +0000

    9382932dd

--- Hmwk/assignment3/prog.s
+++ Hmwk/assignment3/prog.s
@@ -1,6 +1,6 @@
 	.global main
 main:
-	MOV r2, #111
+	MOV r2, #120
 	MOV r3, #5
 	MOV r4, #0
 	MOV r5, #0
@@ -11,17 +11,18 @@ main:
 	MOV r1, r2
 divmod:
 	CMP r1, r3
-	BGT scale
+	BLT div
 scale:
 	MOV r6, #1
-	MLA r7, r3, r6
-	MLA r9, r7, r8
+	MUL r7, r3, r6
+	MUL r9, r7, r8
 	CMP r1, r9
-	BGT scale2
+	BLT div
 scale2:
-	MLA r6, r6, r8
-	MLA r7, r3, r6
-	MLA r9, r7, r8
+	MOV r10, r6
+	MUL r6, r10, r8
+	MUL r7, r3, r6
+	MUL r9, r7, r8
 	CMP r1, r9
 	BGT scale2
 div:
@@ -34,10 +35,12 @@ div:
 	CMP r4, #1
 	BEQ last
 	BAL end
-last
+last:
 	MOV r5, r0
 	MOV r0, r1
 	MOV r1, r5
 end:
+	SUB r0, #1
 	MOV r7, #1
 	SWI 0
+
