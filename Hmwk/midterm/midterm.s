0669c3ed46351dbe8d23f502e6ae9adcce358900
joshuacamacho <joshuajcamacho@gmail.com>
2014-10-27 09:56:31 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- Hmwk/midterm/midterm.s
+++ Hmwk/midterm/midterm.s
@@ -2,12 +2,16 @@
 	menutext: .asciz "Main Menu\n-------------\n1. Problem 1\n2. Problem 2\n3. Problem 3\nEnter the problem number\n"
 .text
 
-.globl main
-main:
+mainmenu:
 	str lr, [sp,#-4]!
 	ldr r0, address_of_menutext
 	bl printf
 	ldr lr, [sp], #+4
+	bx lr
+
+.globl main
+main:
+	bl mainmenu
 	bx lr
 
 address_of_menutext: .word menutext
