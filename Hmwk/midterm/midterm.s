5283a7df105094d7e574976b32e9c1df52d3b846
joshuacamacho <joshuajcamacho@gmail.com>
2014-10-27 11:46:44 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- Hmwk/midterm/midterm.s
+++ Hmwk/midterm/midterm.s
@@ -2,7 +2,8 @@
 	menutext: .asciz "Main Menu\n-------------\n1. Problem 1\n2. Problem 2\n3. Problem 3\nEnter the problem number\n"
 	format: .asciz "%d"
 	problem1text: .asciz "\n\nEnter pay rate\n"
-	problem1text2: .asciz "\n\nEnter hours worked\n"
+	problem1text2: .asciz "Enter hours worked\n"
+	problem1text3: .asciz "Gross pay $%d"
 	problem2text: .asciz "Problem 2\n"
 	problem3text: .asciz "Problem 3\n"
 	testformat: .asciz "you typed %d"
@@ -23,7 +24,7 @@ problem1:
 	sub sp, sp, #4
 	mov r1, sp
 	bl scanf         @get payrate
-	ldr r0, address_of_problem2text
+	ldr r0, address_of_problem1text2
 	
 	bl printf        @ask for hours
 	ldr r0, address_of_format
@@ -36,6 +37,9 @@ problem1:
 	ldr r1, [sp]
 	add sp, sp, #+4
 	
+	MUL r1, r0, r1
+	ldr r0, address_of_problem1text3
+	bl printf
 	ldr lr, [sp], #+4
 	bx lr
 problem2:
@@ -83,5 +87,6 @@ address_of_format: .word format
 address_of_testformat: .word testformat
 address_of_problem1text: .word problem1text
 address_of_problem1text2: .word problem1text2
+address_of_problem1text3: .word problem1text3
 address_of_problem2text: .word problem2text
 address_of_problem3text: .word problem3text
