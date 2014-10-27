052c1aad5c680d4a1a81a9bf1e2dc317878a54b8
joshuacamacho <joshuajcamacho@gmail.com>
2014-10-27 10:12:13 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- Hmwk/midterm/midterm.s
+++ Hmwk/midterm/midterm.s
@@ -1,5 +1,7 @@
 .data
 	menutext: .asciz "Main Menu\n-------------\n1. Problem 1\n2. Problem 2\n3. Problem 3\nEnter the problem number\n"
+	format: .asciz "%d"
+	problem1text: .asciz "Problem 1"
 .text
 
 mainmenu:
@@ -8,11 +10,25 @@ mainmenu:
 	bl printf
 	ldr lr, [sp], #+4
 	bx lr
+	
+
 
 .globl main
 main:
 	bl mainmenu
+	str lr, [sp,#-4]
+	sub sp, sp, #4
+	mov r1, sp
+	ldr r0, 
+	bl scanf
+	ldr r0, [sp]
+	bl printf
+	add sp, sp, #+4    /* Discard the integer read by scanf */
+    ldr lr, [sp], #+4  /* Pop the top of the stack and put it in lr */
+	
+	
 	bx lr
 
 address_of_menutext: .word menutext
+address_of_format: .word format
 	
