dcb8e90923ff56f13bf42d169dc5b9ccedb65d5b
joshuacamacho <joshuajcamacho@gmail.com>
2014-10-27 18:21:41 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- Hmwk/midterm/midterm.s
+++ Hmwk/midterm/midterm.s
@@ -87,13 +87,13 @@ badend:
 	ldr lr, [sp], #+4
 	bx lr
 problem2:
-	str lr, [sp,#-4]!
+	push{lr}
 	ldr r0, address_of_problem2text
 	bl printf
 	ldr lr, [sp], #+4
 	bx lr
 problem3:
-	str lr, [sp,#-4]!
+	push{lr}
 	ldr r0, address_of_problem3text
 	bl printf
 	ldr lr, [sp], #+4
