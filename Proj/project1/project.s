a2b1486d1922dc6b94496b5d29f2725b8f945cd5
Josh <joshuajcamacho@gmail.com>
2014-11-02 20:23:18 -0800

    vccvcvccvvccv

--- Proj/project1/project.s
+++ Proj/project1/project.s
@@ -3,6 +3,7 @@
 @ 11/2/14
 .data
 	introtext: .asciz "\n\n\n                            THE DUNGEON OF DESPAIR\n      .                                                      .\n        .n                   .                 .                  n.\n  .   .dP                  dP                   9b                 9b.    .\n 4    qXb         .       dX                     Xb       .        dXp     t\ndX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb\n9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP\n 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP\n  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'\n    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'\n        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~\n                        )b.  .dbo.dP'`v'`9b.odb.  .dX(\n                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.\n                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb\n                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb\n                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP\n                     `'      9XXXXXX(   )XXXXXXP      `'\n                              XXXX X.`v'.X XXXX\n                              XP^X'`b   d'`X^XX\n                              X. 9  `   '  P )X\n                              `b  `       '  d'\n                               `             '\n                            Press Enter to Continue\n"
+	spacing .asciz "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 	charformat: .asciz "%c"
 
 .text
@@ -15,16 +16,20 @@
 main:
 	push {lr}
 	ldr r0, address_of_introtext
-	bl printf
+	bl printf                         @ Display Intro text
 	ldr r0, address_of_charformat
 	sub sp, sp, #4
 	mov r1, sp
-	bl scanf
+	bl scanf                          @ Ask for enter to be pressed
 	add sp, sp, #+4
+	ldr r0, address_of_spacing
+	bl printf                         @ Put spacing
+
 
 end:
 	pop {lr}
 	bx lr
 
 address_of_introtext: .word introtext
+address_of_spacing .word spacing
 address_of_charformat: .word charformat
