7fdd123704841bef8ac8a95a43dd43b58e252e87
Joshua Camacho <joshuajcamacho@gmail.com>
2014-12-12 10:01:34 -0800

    fgsfsfsfs

--- final/prob1.s
+++ final/prob1.s
@@ -1,11 +1,10 @@
 .data
-/* text */
-intro: .asciz"I have a number between 1 and 1000\nCan you guess my number?\nYou will be given a maximum of 10 guesses."
-in1: .asciz"Try #%d .\n"
-out1: .asciz"1. Congratulations, You guessed the number!\n"
-out2: .asciz"2. Too low.  Try again.\n"
-out3: .asciz"3. Too High. Try again.\n"
-out4: .asciz"4. Too many tries, you lose.\n"
+introtext: .asciz "I have a number between 1 and 1000\nCan you guess my number?\nYou will be given a maximum of 10 guesses."
+youwintext: .asciz "1. You guessed my number, you win!\n"
+toolowtext: .asciz "2. Too low.  Try again.\n"
+toohightext: .asciz "3. Too High. Try again.\n"
+youlosetext: .asciz "4. Too many tries, you lose.\n"
+trytext: .asciz "\nTry #%d .\n"
 qYN: .asciz"Would you like to play again (y)es or (n)o?\n"
 inv: .asciz"Invalid input\n"
 /* scan format */
@@ -19,9 +18,9 @@ in: .word 0
 .align 4
 ansYN: .word 0
 .align 4
-y: .asciz"y"
+y: .asciz "y"
 .align 4
-n: .asciz"n"
+n: .asciz "n"
 
 .text
 	.global problem1
@@ -39,10 +38,10 @@ p1start:
 	mov r5, r1			@r5=ans
 /*	ldr r0, =scan
 	bl printf*/
-	ldr r0, =intro
+	ldr r0, =introtext
 	bl printf			@output intro
 p1loop:
-	ldr r0, =in1
+	ldr r0, =trytext
 	mov r1, r7
 	bl printf			@output try counter
 	ldr r0, =scan
@@ -52,7 +51,7 @@ p1loop:
 	ldr r1, [r1]		@r1=in
 	mov r3, r1			@r3=in
 	cmp r7, #10
-	ldreq r0, =out4
+	ldreq r0, =youlosetext
 	bleq printf
 	beq Question
 	mov r2, r5
@@ -61,11 +60,11 @@ p1loop:
 	beq bingo
 	bgt high
 low:
-	ldr r0, =out2
+	ldr r0, =toolowtext
 	bl printf
 	b p1count
 high:
-	ldr r0, =out3
+	ldr r0, =toohightext
 	bl printf
 	b p1count
 p1count:
@@ -73,7 +72,7 @@ p1count:
 	cmp r7, #11
 	ble p1loop
 bingo:
-	ldr r0, =out1
+	ldr r0, =youwintext
 	bl printf
 Question:
 	ldr r0, =qYN
