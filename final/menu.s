bdd87ba75a0f0a21f41d4f9c1cf75798c4adb919
Joshua Camacho <joshuajcamacho@gmail.com>
2014-12-12 08:40:30 -0800

    fsfsf

---
+++ final/menu.s
@@ -0,0 +1,87 @@
+@ Joshua Camacho
+@ CSC-11 Final
+@ 12/12/14
+
+.data
+@ Main Menu 
+.balign 4
+introtext: .asciz "Main Menu\n"
+.balign 4
+prob1: .asciz "1. Problem 1\n"
+.balign 4
+prob2: .asciz "2. Problem 2\n"
+.balign 4
+prob3: .asciz "3. Problem 3\n"
+.balign 4
+prob4: .asciz "4. Problem 4\n"
+.balign 4
+probexit: .asciz "0. Exit\n"
+.balign 4
+scanMenu: .asciz "%d"
+.balign 4
+inputchoice: .word 0
+.balign 4
+repeat: .asciz "Input %d\n"
+.balign 4
+exit: .asciz "Press %d to exit\n"
+
+
+.text
+	.global main
+main:
+	push {r4, lr}
+menu:
+	ldr r0, =introtext
+	bl printf
+	ldr r0, =prob1
+	bl printf
+	ldr r0, =prob2
+	bl printf
+	ldr r0, =prob3
+	bl printf
+	ldr r0, =prob4
+	bl printf		@ show menu
+	ldr r0, =probexit
+	bl printf
+
+	ldr r0, =scanMenu
+	ldr r1, =inputchoice
+	bl scanf		@ User choose problem
+	ldr r0, =repeat
+	ldr r1, =inputchoice
+	ldr r1, [r1]
+	mov r5, r1
+	bl printf
+
+	mov r1, r5		 
+	cmp r1, #1
+	bleq prog1  @ Branch to Problem 1
+	beq menu
+
+	mov r1, r5		
+	cmp r1, #2
+	bleq prog2  @ Branch to Problem 2
+	beq menu
+
+	mov r1, r5		
+	cmp r1, #3
+	bleq prog3  @ Branch to Problem 3
+	beq menu
+
+	mov r1, r5		
+	cmp r1, #4
+	bleq prog4  @ Branch to Problem 4
+	beq menu
+	bgt end
+
+end:
+	ldr r0, =exit
+	mov r1, r5
+	bl printf
+	pop {r4, lr}
+	bx lr
+
+/* external */
+.global printf
+.global scanf
+.global strcmp
